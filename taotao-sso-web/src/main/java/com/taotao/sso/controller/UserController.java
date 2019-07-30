cb098b5b40320d65557614ef17b9cebf60d4081d
chinaliuweijie <chinaliuweijie@163.com>
2019-07-30 18:30:26 +0800

    购物车功能完成和生成订单

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.CookieUtils;
+import com.taotao.common.utils.JsonUtil;
 import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
  
@@ -63,13 +65,37 @@ public class UserController {
 
     
     
-    @RequestMapping(value = "/user/token/{token}",method = RequestMethod.GET)
-    @ResponseBody
-    public TaotaoResult getUserByToken(@PathVariable String token){
-    	TaotaoResult result = userService.getUserByToken(token);
-    	return result;
+//    @RequestMapping(value = "/user/token/{token}",method = RequestMethod.GET)
+//    @ResponseBody
+//    public TaotaoResult getUserByToken(@PathVariable String token){
+//    	TaotaoResult result = userService.getUserByToken(token);
+//    	return result;
+//    }
+    
+    
+//    @RequestMapping(value = "/user/token/{token}",method = RequestMethod.GET)  
+//    @ResponseBody  
+//    public String getUserByToken(@PathVariable String token,String callback){  
+//        TaotaoResult result = userService.getUserByToken(token);  
+//        if(StringUtils.isNotBlank(callback)){
+//        	return callback+"("+JsonUtil.objectToJson(result)+");";
+//        }
+//        return JsonUtil.objectToJson(result);  
+//    }
+    
+    @RequestMapping(value = "/user/token/{token}",method = RequestMethod.GET)  
+    @ResponseBody  
+    public Object getUserByToken(@PathVariable String token,String callback){  
+        TaotaoResult result = userService.getUserByToken(token);  
+        if(StringUtils.isNotBlank(callback)){
+        	MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+        	mappingJacksonValue.setJsonpFunction(callback);
+        	return mappingJacksonValue;
+        }
+        return result;  
     }
-
+    
+    
     
     @RequestMapping(value = "/user/logout/{token}",method = RequestMethod.GET)
     @ResponseBody
